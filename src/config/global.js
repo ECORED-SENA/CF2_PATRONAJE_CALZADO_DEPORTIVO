d24a39d56f30d637a19da59fad37da24515e907f
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-06-20 18:31:03 -0500

    Vinculación PDF - MD

--- src/config/global.js
+++ src/config/global.js
@@ -143,7 +143,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/52450481_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
